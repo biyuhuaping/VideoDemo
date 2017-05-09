017b35e76bfd1fb3313d1aef36c5654874327b14
zhoubo <zhoubo@xbniao.com>
2017-05-09 11:16:09 +0800

    添加了简单滤镜功能

--- VideoDemo/ViewController/VideoDetail(视频详情)/LZVideoDetailsVC.m
+++ VideoDemo/ViewController/VideoDetail(视频详情)/LZVideoDetailsVC.m
@@ -10,9 +10,11 @@
 #import "LZVideoEditClipVC.h"
 #import <AssetsLibrary/AssetsLibrary.h>
 #import "LZVideoTools.h"
+#import "SCWatermarkOverlayView.h"
 
-@interface LZVideoDetailsVC ()
-@property (strong, nonatomic) IBOutlet SCVideoPlayerView *videoPlayerView;
+@interface LZVideoDetailsVC ()<SCPlayerDelegate>
+@property (strong, nonatomic) IBOutlet SCSwipeableFilterView *filterSwitcherView;
+@property (strong, nonatomic) SCPlayer *player;
 @end
 
 @implementation LZVideoDetailsVC
@@ -21,18 +23,49 @@
     [super viewDidLoad];
     self.title = LZLocalizedString(@"video_details", nil);
     [self configNavigationBar];
-    [self.videoPlayerView.player setLoopEnabled:YES];
+    
+    _player = [SCPlayer player];
+    [self.player setLoopEnabled:YES];
+
+    if ([[NSProcessInfo processInfo] activeProcessorCount] > 1) {
+//        self.filterSwitcherView.contentMode = UIViewContentModeScaleAspectFill;
+        
+        SCFilter *emptyFilter = [SCFilter emptyFilter];
+        emptyFilter.name = @"#nofilter";
+        
+        self.filterSwitcherView.filters = @[
+                                            emptyFilter,
+                                            [SCFilter filterWithCIFilterName:@"CIPhotoEffectNoir"],
+                                            [SCFilter filterWithCIFilterName:@"CIPhotoEffectChrome"],
+                                            [SCFilter filterWithCIFilterName:@"CIPhotoEffectInstant"],
+                                            [SCFilter filterWithCIFilterName:@"CIPhotoEffectTonal"],
+                                            [SCFilter filterWithCIFilterName:@"CIPhotoEffectFade"],
+                                            // Adding a filter created using CoreImageShop
+                                            [SCFilter filterWithContentsOfURL:[[NSBundle mainBundle] URLForResource:@"a_filter" withExtension:@"cisf"]],
+                                            [self createAnimatedFilter]
+                                            ];
+        self.player.SCImageView = self.filterSwitcherView;
+//        [self.filterSwitcherView addObserver:self forKeyPath:@"selectedFilter" options:NSKeyValueObservingOptionNew context:nil];
+    } else {
+        SCVideoPlayerView *playerView = [[SCVideoPlayerView alloc] initWithPlayer:self.player];
+        playerView.playerLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+        playerView.frame = self.filterSwitcherView.frame;
+        playerView.autoresizingMask = self.filterSwitcherView.autoresizingMask;
+        [self.filterSwitcherView.superview insertSubview:playerView aboveSubview:self.filterSwitcherView];
+        [self.filterSwitcherView removeFromSuperview];
+    }
+
 }
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-    [self.videoPlayerView.player setItemByAsset:self.recordSession.assetRepresentingSegments];
-    [self.videoPlayerView.player play];
+    [self.player setItemByAsset:self.recordSession.assetRepresentingSegments];
+    [self.player play];
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
     [super viewWillDisappear:animated];
-    [self.videoPlayerView.player pause];
+    [self.player pause];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -46,7 +79,7 @@
     UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
     button.titleLabel.font = [UIFont systemFontOfSize:15];
     button.selected = NO;
-    [button setTitle:LZLocalizedString(@"next", nil) forState:UIControlStateNormal];
+    [button setTitle:@"保存到本地" forState:UIControlStateNormal];
     [button setTitleColor:UIColorFromRGB(0xffffff, 1) forState:UIControlStateNormal];
     [button sizeToFit];
     button.frame = CGRectMake(0, 0, CGRectGetWidth(button.bounds), 40);
@@ -58,6 +91,9 @@
 
 #pragma mark - Event
 - (void)navbarRightButtonClickAction:(UIButton*)sender {
+    [self saveToCameraRoll];
+    return;
+    
     NSURL *tempPath = [LZVideoTools filePathWithFileName:@"ConponVideo.mp4"];
 
 //    4.导出
@@ -82,6 +118,102 @@
     LZVideoEditClipVC * vc = [[LZVideoEditClipVC alloc] initWithNibName:@"LZVideoEditClipVC" bundle:nil];
     vc.recordSession = self.recordSession;
     [self.navigationController pushViewController:vc animated:YES];
+}
+
+//创建动态滤镜
+- (SCFilter *)createAnimatedFilter {
+    SCFilter *animatedFilter = [SCFilter emptyFilter];
+    animatedFilter.name = @"Animated Filter";
+    
+    SCFilter *gaussian = [SCFilter filterWithCIFilterName:@"CIGaussianBlur"];
+    SCFilter *blackAndWhite = [SCFilter filterWithCIFilterName:@"CIColorControls"];
+    
+    [animatedFilter addSubFilter:gaussian];
+    [animatedFilter addSubFilter:blackAndWhite];
+    
+    double duration = 0.5;
+    double currentTime = 0;
+    BOOL isAscending = YES;
+    
+    Float64 assetDuration = CMTimeGetSeconds(_recordSession.assetRepresentingSegments.duration);
+    
+    while (currentTime < assetDuration) {
+        if (isAscending) {
+            [blackAndWhite addAnimationForParameterKey:kCIInputSaturationKey startValue:@1 endValue:@0 startTime:currentTime duration:duration];
+            [gaussian addAnimationForParameterKey:kCIInputRadiusKey startValue:@0 endValue:@10 startTime:currentTime duration:duration];
+        } else {
+            [blackAndWhite addAnimationForParameterKey:kCIInputSaturationKey startValue:@0 endValue:@1 startTime:currentTime duration:duration];
+            [gaussian addAnimationForParameterKey:kCIInputRadiusKey startValue:@10 endValue:@0 startTime:currentTime duration:duration];
+        }
+        
+        currentTime += duration;
+        isAscending = !isAscending;
+    }
+    
+    return animatedFilter;
+}
+
+//保存到相机卷
+- (void)saveToCameraRoll {
+    self.navigationItem.rightBarButtonItem.enabled = NO;
+    SCFilter *currentFilter = [self.filterSwitcherView.selectedFilter copy];
+    [_player pause];
+    
+    SCAssetExportSession *exportSession = [[SCAssetExportSession alloc] initWithAsset:self.recordSession.assetRepresentingSegments];
+    exportSession.videoConfiguration.filter = currentFilter;
+    exportSession.videoConfiguration.preset = SCPresetHighestQuality;
+    exportSession.audioConfiguration.preset = SCPresetHighestQuality;
+    exportSession.videoConfiguration.maxFrameRate = 35;
+    exportSession.outputUrl = self.recordSession.outputUrl;
+    exportSession.outputFileType = AVFileTypeMPEG4;
+//    exportSession.delegate = self;
+    exportSession.contextType = SCContextTypeAuto;
+    
+//    exportView.hidden = NO;
+//    exportView.alpha = 0;
+    
+    SCWatermarkOverlayView *overlay = [SCWatermarkOverlayView new];
+    overlay.date = self.recordSession.date;
+    exportSession.videoConfiguration.overlay = overlay;
+    
+    CFTimeInterval time = CACurrentMediaTime();
+    __weak typeof(self) wSelf = self;
+    [exportSession exportAsynchronouslyWithCompletionHandler:^{
+        __strong typeof(self) strongSelf = wSelf;
+        
+        if (!exportSession.cancelled) {
+            NSLog(@"Completed compression in %fs", CACurrentMediaTime() - time);
+        }
+        
+        if (strongSelf != nil) {
+            [strongSelf.player play];
+            strongSelf.navigationItem.rightBarButtonItem.enabled = YES;
+            
+//            [UIView animateWithDuration:0.3 animations:^{
+//                strongSelf.exportView.alpha = 0;
+//            }];
+        }
+        
+        NSError *error = exportSession.error;
+        if (exportSession.cancelled) {
+            NSLog(@"Export was cancelled");
+        } else if (error == nil) {
+            [[UIApplication sharedApplication] beginIgnoringInteractionEvents];
+            [exportSession.outputUrl saveToCameraRollWithCompletion:^(NSString * _Nullable path, NSError * _Nullable error) {
+                [[UIApplication sharedApplication] endIgnoringInteractionEvents];
+                
+                if (error == nil) {
+                    [[[UIAlertView alloc] initWithTitle:@"Saved to camera roll" message:@"" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+                } else {
+                    [[[UIAlertView alloc] initWithTitle:@"Failed to save" message:error.localizedDescription delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+                }
+            }];
+        } else {
+            if (!exportSession.cancelled) {
+                [[[UIAlertView alloc] initWithTitle:@"Failed to save" message:error.localizedDescription delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+            }
+        }
+    }];
 }
 
 @end
