8a2731e488373a61d86b523009293baf0161c288
ZB <biyuhuaping@163.com>
2025-04-13 21:52:34 +0800

    no message

--- VideoDemo/Tools/LZVideoTools.h
+++ VideoDemo/Tools/LZVideoTools.h
@@ -12,35 +12,24 @@
 
 @interface LZVideoTools : NSObject
 
-/**
- 视频压缩+剪切+导出
- 
- @param selectSegment 所选视频资源
- @param filePath 文件路径
- @param completion 完成回调
- */
+/// 视频压缩+剪切+导出
+/// @param selectSegment 所选视频资源
+/// @param filePath 文件路径
+/// @param completion 完成回调
 + (void)cutVideoWith:(SCRecordSessionSegment *)selectSegment filePath:(NSURL *)filePath completion:(void (^)(void))completion;
 
 
-/**
- 导出视频
- 
- @param asset 视频资源
- @param videoComposition 视频合成物
- @param filePath 文件路径
- @param range 时长范围
- @param completion 完成回调
- */
+/// 导出视频
+/// @param asset 视频资源
+/// @param videoComposition 视频合成物
+/// @param filePath 文件路径
+/// @param range 时长范围
+/// @param completion 完成回调
 + (void)exportVideo:(AVAsset *)asset videoComposition:(AVVideoComposition *)videoComposition filePath:(NSURL *)filePath timeRange:(CMTimeRange)range completion:(void (^)(NSURL *savedPath))completion;
 
 
-
-/**
- 配置文件路径
-
- @param fileName 文件名称
- @return 文件路径
- */
+/// 配置文件路径
+/// @param fileName 文件名称
 + (NSURL *)filePathWithFileName:(NSString *)fileName;
 
 @end
--- VideoDemo/ViewController/VideoEdit(视频编辑)/LZVideoEditAuxiliary.h
+++ VideoDemo/ViewController/VideoEdit(视频编辑)/LZVideoEditAuxiliary.h
@@ -13,41 +13,27 @@
 
 @interface LZVideoEditAuxiliary : NSObject
 
-/**
- 获取当前segment
- 
- @param recordSegments <#recordSegments description#>
- @param idx <#idx description#>
- @return <#return value description#>
- */
+/// 获取当前segment
+/// @param recordSegments <#recordSegments description#>
+/// @param idx <#idx description#>
 - (SCRecordSessionSegment *)getCurrentSegment:(NSArray *)recordSegments index:(NSInteger)idx;
 
 
-/**
- 获取视频当前总时间长度
- 
- @param recordSegments <#recordSegments description#>
- @return <#return value description#>
- */
+/// 获取视频当前总时间长度
+/// @param recordSegments <#recordSegments description#>
 - (float)getAllVideoTimesRecordSegments:(NSArray *)recordSegments;
 
 
-/**
- 更新进度条
- 
- @param progressBar <#progressBar description#>
- @param recordSegments <#recordSegments description#>
- */
+/// 更新进度条
+/// @param progressBar <#progressBar description#>
+/// @param recordSegments <#recordSegments description#>
 - (void)updateProgressBar:(ProgressBar *)progressBar :(NSArray *)recordSegments;
 
 
-/**
- 更新剪切片段
- 
- @param trimmerView <#trimmerView description#>
- @param recordSegments <#recordSegments description#>
- @param currentSelected <#currentSelected description#>
- */
+/// 更新剪切片段
+/// @param trimmerView <#trimmerView description#>
+/// @param recordSegments <#recordSegments description#>
+/// @param currentSelected <#currentSelected description#>
 - (void)updateTrimmerView:(SAVideoRangeSlider *)trimmerView recordSegments:(NSArray *)recordSegments index:(NSInteger)currentSelected;
 
 @end
